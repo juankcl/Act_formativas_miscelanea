1d3cc63c91dc9a1695b91973f717699ab4a71057
juankcl <esjuankarlos1@gmail.com>
2019-06-10 13:22:54 -0500

    Main colocar opciones

--- main.c
+++ main.c
@@ -3,5 +3,10 @@
 int main()
 {
         printf("Hola mundo");
+        printf("1. Factorial");
+        printf("2. Conversor Decimal a Binario");
+        printf("3. Numeros Primos");
+        printf("4. Suma-Multiplicacion");
+        printf("5. Fibonacci");
         return 0;
 }
